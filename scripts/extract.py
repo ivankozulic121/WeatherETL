88c1f7188f6b4a2efd85f1df153b36760dcaeea8
ivank <ivankozulic121@gmail.com>
2026-02-26 00:05:05 +0100

    implemented weather data extraction

--- scripts/extract.py
+++ scripts/extract.py
@@ -10,7 +10,6 @@ def extract_towns():
         url = f"https://geocoding-api.open-meteo.com/v1/search?name={town}&count=10&language=en&format=json"
         response = requests.get(url)
         data = response.json()
-        #print(data)
         for item in data["results"]:
             towns_data.append(
                 {
@@ -23,5 +22,27 @@ def extract_towns():
             )
 
     return towns_data
+
+
+def extract_weather_data():
+    towns_data = extract_towns()
+    weather_data = []
+    baseURL = "https://api.open-meteo.com/v1/forecast"
+
+    for town in towns_data:
+        response = requests.get(f"{baseURL}?latitude={town['latitude']}&longitude={town['longitude']}&current=temperature_2m,relative_humidity_2m,surface_pressure,wind_speed_10m")
+        data = response.json()
+        current_weather = data.get("current")
+        weather_data.append(
+            {
+                "temperature": current_weather.get('temperature_2m'),
+                "humidity": current_weather.get('relative_humidity_2m'),
+                "pressure": current_weather.get('surface_pressure'),
+                "wind_speed": current_weather.get('wind_speed_10m'),
+            }
+        )
+    return weather_data
+
+
 
 
